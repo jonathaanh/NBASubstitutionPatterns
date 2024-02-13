dfcbe60735650b2c1aba7d401e1d050c667ceac9
Jonathan Hsu <johnnhsu1031@gmail.com>
2024-02-12 17:34:15 -0800

    Data update

--- code/python/scraper.py
+++ code/python/scraper.py
@@ -137,7 +137,7 @@ def process_plus_minus(plus_minus_link, isHomeGame, num_overtimes, players):
 def main():
 
 	today = datetime.now().date()
-	years = ["2020"]
+	years = ["2021"]
 
 	for year in years:
 		print("DOING YEAR " + year)
